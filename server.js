c6fc9146b5199d54f311707a7cdd8157d87a7f3f
Almog Skalka <almogsk14@gmail.com>
2023-09-02 12:24:07 +0300

    Adding mongodb and updating gitignore

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => console.log("Connected!"));
+
 //routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  throw new Error("regular get not working");
 });
-
+app.get("/blobing", (req, res) => {
+  res.send("blobing something!");
+  throw new Error("blobing get not working");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
